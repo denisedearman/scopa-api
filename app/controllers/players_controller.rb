fcf2bf6026364a442a47a9df62d83dbf90c3b022
denisedearman <dearmandenise@gmail.com>
2017-11-24 13:12:45 -0500

    modified appliication_controller and added a show method to the player controller

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -1,5 +1,5 @@
 class PlayersController < ApplicationController
-
+  before_action :set_player, only: [:show]
   def create
     @player = Player.new(player_params)
 
@@ -15,7 +15,15 @@ class PlayersController < ApplicationController
     render json: @players.as_json
   end
 
+  def show
+    render json: @player.as_json
+  end
+
   private
+
+  def set_player
+    @player = Player.find_by(id: params[:id])
+  end
 
   def player_params
     params.require(:player).permit(:name)
