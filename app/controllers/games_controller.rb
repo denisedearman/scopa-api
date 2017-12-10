1087ae05cabe73eb2de84eb553a7ff676db1f0f8
denisedearman <dearmandenise@gmail.com>
2017-12-10 13:20:11 -0500

    made the game that is returned include the players of the game

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,6 +1,7 @@
 class GamesController < ApplicationController
-
+  before_action :set_game, only: [:show]
   def index
+    render json: Game.all
   end
 
   def create
@@ -8,12 +9,21 @@ class GamesController < ApplicationController
     @game.complete = false;
     binding.pry
     if @game.save
-      render json: @game
+      render json: {"game_id" => @game.id,"player_1" => @game.players[0].name, "player_2" => @game.players[1].name}.to_json
     else
       render json: {error: 'Unable to save Game.'}
     end
   end
+
+  def show
+    render json: {"game_id" => @game.id, "player_1" => @game.players[0].name, "player_2" => @game.players[1].name}.to_json
+  end
+
   private
+
+  def set_game
+    @game = Game.find_by(id: params[:id])
+  end
 
   def game_params
     params.require(:game).permit(:player_1, :player_2, cards: [])
