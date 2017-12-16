dc0904afc5e7c39ca63f32df3a6761d40a6fb481
denisedearman <dearmandenise@gmail.com>
2017-12-16 15:25:03 -0500

    added game/player/edit route

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -1,5 +1,5 @@
 class PlayersController < ApplicationController
-  before_action :set_player, only: [:show]
+  before_action :set_player, only: [:show, :edit]
   def create
     @player = Player.new(player_params)
 
@@ -10,17 +10,36 @@ class PlayersController < ApplicationController
     end
   end
 
+  def edit
+    if params[:game_id]
+      game = Game.find_by(id: params[:game_id])
+      current_player = game.get_current_player
+      if current_player.id == @player.id
+        render json: {"hand" => @player.hand, "table" => game.on_table}.to_json
+      else
+        render json: "Not your turn!"
+      end
+    end
+  end
+
   def index
     render json: Player.all
   end
 
   def show
     if params[:game_id]
-      render json: {"player_id" => @player.id, "name" => @player.name}.to_json
+      game = Game.find_by(id: params[:game_id])
+      current_player = game.get_current_player
+      if current_player.id == @player.id
+        render json: {"player_id" => @player.id, "name" => @player.name, "turn" => true}.to_json
+      else
+        render json: {"player_id" => @player.id, "name" => @player.name, "turn" => false}.to_json
+      end
     else
       render json: @player
     end
   end
+
 
   private
 
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   resources :players, except: [:new, :edit, :update, :destroy]
   resources :games, except: [:new, :edit, :update, :destroy]
   resources :games, only:[] do
-    resources :players, only:[:show]
+    resources :players, only:[:show, :edit, :update]
   end
   get '/games/:id/play' => 'games#play'
 end
